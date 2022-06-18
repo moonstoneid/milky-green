992cc89f49641f4eecee05dfd06ed7e1f7d304be
Michael Netter <mail@michaelnetter.com>
2022-06-18 16:49:28 +0200

    Add Web3Modal and WalletConnect support

--- backend/src/main/resources/static/js/base.js
+++ backend/src/main/resources/static/js/base.js
@@ -1,29 +1,80 @@
 import { ethers } from '/js/ethers.js';
 
-const ethereum = window.ethereum;
+const Web3Modal = window.Web3Modal.default;
+const WalletConnectProvider = window.WalletConnectProvider.default;
 
 let signer;
+let web3Modal;
+let web3ModalProvider;
+let provider;
 
-function connectWallet() {
-    if (!ethereum) {
-        showError('You need to install MetaMask.');
-        return;
+// Initializes Web3Modal
+function initWeb3Modal() {
+    const providerOptions = {
+        walletconnect: {
+            package: WalletConnectProvider,
+            options: {
+                infuraId: "b5f225423daa44d092673365889de8b7",
+            }
+        }
+    };
+
+    web3Modal = new Web3Modal({
+        cacheProvider: false, // optional
+        providerOptions, // required
+        disableInjectedProvider: false
+    });
+}
+
+// Pops up the Web3Modal and connects to the wallet the user has selected
+async function connectWallet() {
+
+    try {
+        // Open modal
+        web3ModalProvider = await web3Modal.connect();
+    } catch(e) {
+        console.log("Could not get a wallet connection.");
+        showConnectWalletButton();
+    return;
     }
 
-    const provider = new ethers.providers.Web3Provider(ethereum);
-
+    provider = new ethers.providers.Web3Provider(web3ModalProvider);
     signer = provider.getSigner();
 
-    provider.send('eth_requestAccounts', [])
-        .then(() => {
-            hideError();
+    provider.listAccounts()
+        .then(async result => {
             hideConnectWalletButton();
-            showContent();
+            showInfo("Please check your wallet to sign the message.");
+            await loginInWithEthereum();
         })
-        .catch(() => {
-            showError('Connecting wallet was rejected.');
+        .catch(error => {
+            hideInfo();
             showConnectWalletButton();
-        });
+        })
+}
+
+// Submits the login form
+async function loginInWithEthereum() {
+      // Create and sign message
+      const address = await getAccountAddress();
+      const { chainId } = await provider.getNetwork();
+      const message = await createLoginMessage(chainId, address);
+      const signature = await signMessage(message);
+
+      document.getElementById('siwe-message').value = window.btoa(message);
+      document.getElementById('siwe-signature').value = window.btoa(signature);
+
+      const form = document.getElementById('login-form');
+      form.submit();
+}
+
+// Creates a valid EIP-4361 string
+async function createLoginMessage(chainId, address) {
+      const query = 'chain_id=' + chainId + '&address=' + address;
+      const res = await fetch('/login-message?' + query, {
+        credentials: 'include',
+      });
+      return await res.text();
 }
 
 async function getAccountAddress() {
@@ -34,14 +85,14 @@ async function signMessage(message) {
     return await signer.signMessage(message);
 }
 
-function showError(message) {
-    const div = document.getElementById('error-div');
+function showInfo(message) {
+    const div = document.getElementById('info-div');
     div.innerHTML = message;
     div.style.display = 'block';
 }
 
-function hideError() {
-    const div = document.getElementById('error-div');
+function hideInfo() {
+    const div = document.getElementById('info-div');
     div.style.display = 'none';
 }
 
@@ -55,9 +106,4 @@ function hideConnectWalletButton() {
     button.style.display = 'none';
 }
 
-function showContent() {
-    const div = document.getElementById('content-div');
-    div.style.display = 'block';
-}
-
-export { connectWallet, getAccountAddress, signMessage };
+export { initWeb3Modal, connectWallet };
