5f0f30bb15e3888d11d66bd7d79869efdcc96e3a
Matthias Kellnhofer <matthias@kellnhofer.com>
2022-11-16 20:17:41 +0100

    Migrate to Docker multi-stage build

--- compose-up-debug.sh
+++ compose-up-debug.sh
@@ -1,6 +1,4 @@
-./mvnw package
-
-docker-compose \
+DOCKER_BUILDKIT=1 docker-compose \
   --project-name web3-login \
   --file docker-compose.yml --file docker-compose-debug.yml \
   up --build --detach
